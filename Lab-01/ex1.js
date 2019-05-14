39bbd7f733c17ee38cd1d5e349e02c9e1f589f68
pk21-praveen <pk21.praveen@gmail.com>
2019-05-14 23:29:27 +0530

    updated

--- Lab-01/ex1.js
+++
@@ -1,9 +0,0 @@
-var func_1 = function () {
-    console.log("Hello World !!!");
-}
-
-function execute(myFunction) {
-    myFunction();
-}
-
-execute(func_1);
--- Lab-02/ex3.1.js
+++
@@ -1,9 +0,0 @@
-function fetchUsers() {
-    return fetch('https://api.github.com/users')
-        .then((response) => response.json());
-
-}
-
-fetchUsers.then(function (json) {
-    console.log(json);
-});
